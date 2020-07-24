0fbed4e05f03836fe6a468ff3e3a6ae6c24c0439
lil_gappy_69 <49754258+gaponukz@users.noreply.github.com>
2020-07-25 00:27:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,14 +22,11 @@ class ParseAnswers(object):
         self.driver.find_element_by_xpath('//*[@id="login"]/form/button').click()
 
     def parse(self, index = '1') -> dict:
-        path_to_element = '6'
-        path_to_element = '10' if index == '8' else path_to_element
-        path_to_element = '8' if index == '11' else path_to_element
         sleep(1)
         self.driver.find_element_by_xpath('/html/body/nav/div/div/div/div[4]/a').click()
         self.driver.find_element_by_xpath(f'/html/body/section[2]/div/div/div[{index}]/div/div[2]/a').click()
         sleep(1)
-        self.driver.find_element_by_xpath(f'/html/body/section[2]/div/div[{path_to_element}]/a[2]').click()
+        self.driver.find_element_by_css_selector('body > section.course-detail-page > div > div.course-rules-page__btns > a.btn.btn-blue-transparent.course-rules-page__btn').click()
         num = self.driver.current_url.split('/quiz/')[-1]
 
         return parse_answers(self.driver.page_source), num
@@ -62,45 +59,45 @@ if __name__ == "__main__":
     else:
         for j in range(1, epoch + 1):
             os.mkdir(PATH_TO_DATA + str(j))
-            for i in range(1, 18):
-                if not i in black_list:
-                    try:
-                        parser = ParseAnswers()
-                        parser.login(username, password)
-                        data, num = parser.parse(index = str(i))
-                        parser.close()
+            for i in range(1, 20):
+                try:
+                    parser = ParseAnswers()
+                    parser.login(username, password)
+                    data, num = parser.parse(index = str(i))
+                    parser.close()
 
-                        with open (PATH_TO_DATA + str(j) + '\\' + f'{num}.json', 'w') as f:
-                            f.write(json.dumps(data, indent = 4, sort_keys = True))
-                        
-                        workbook = xlsxwriter.Workbook(PATH_TO_DATA + str(j) + '\\' + f'{file_name[str(i)]}.xlsx') 
-                        worksheet = workbook.add_worksheet()
-                        
-                        row, column = 0, 0
-                        titles = ['Вопрос', 'Все ответи', 'Правильний ответ']
-                        for item in titles: 
-                            worksheet.write(row, column, item) 
-                            column += 1
-
-                        row = 1
-                        for item in data['data']:
-                            column = 0
-                            all_answers = ''
-                            true_answer = ''
-
-                            for answer in item['answers']:
-                                all_answers += answer['title'] + '\n'
-                                if answer['is_true_answer']:
-                                    true_answer = answer['title']
-
-                            worksheet.write(row, column, item['title'])
-                            worksheet.write(row, column + 1, all_answers)
-                            worksheet.write(row, column + 2, true_answer)
-
-                            row += 1
-
-                            workbook.close()
-                    except:
-                        pass    
+                    with open (PATH_TO_DATA + str(j) + '\\' + f'{num}.json', 'w') as f:
+                        f.write(json.dumps(data, indent = 4, sort_keys = True))
                     
-                print(f'Parsed {i} successfully')
+                    workbook = xlsxwriter.Workbook(PATH_TO_DATA + str(j) + '\\' + f'{file_name[str(i)]}.xlsx') 
+                    worksheet = workbook.add_worksheet()
+                    
+                    row, column = 0, 0
+                    titles = ['Вопрос', 'Все ответи', 'Правильний ответ']
+                    for item in titles: 
+                        worksheet.write(row, column, item) 
+                        column += 1
+
+                    row = 1
+                    for item in data['data']:
+                        column = 0
+                        all_answers = ''
+                        true_answer = ''
+
+                        for answer in item['answers']:
+                            all_answers += answer['title'] + '\n'
+                            if answer['is_true_answer']:
+                                true_answer = answer['title']
+
+                        worksheet.write(row, column, item['title'])
+                        worksheet.write(row, column + 1, all_answers)
+                        worksheet.write(row, column + 2, true_answer)
+
+                        row += 1
+
+                    workbook.close()
+
+                    print(f'Parsed {num} successfully')
+
+                except Exception as error:
+                    print(error)
